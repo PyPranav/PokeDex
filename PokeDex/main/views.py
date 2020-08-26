f7d78c4660c31085de2d028232176924f6da228f
PyPranav <59741576+PyPranav@users.noreply.github.com>
2020-08-26 07:57:09 +0530

    ver-2

--- PokeDex/main/views.py
+++ PokeDex/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
-import requests
+# import requests
 import json
+from difflib import get_close_matches
 
 def home(request):
     return render(request, 'home.html')
@@ -11,7 +12,16 @@ def pokemon(request):
     if request.method == "POST":
         name = request.POST.get('search-box')
         print(name)
-        r = requests.get("https://pokeapi.glitch.me/v1/pokemon/"+name).json()
+        with open('static/poke_name.json', "r") as f:
+            data = json.load(f)
+        try:
+            name = int(name)
+            r = list(data.values())[name-1]
+        except Exception:
+            pokemon = list(data.keys())
+            name = get_close_matches(name, pokemon)[0]
+            print(name)
+            r = data[name]
         with open('static/pokemon.json', 'w') as f:
             json.dump(r, f)
         return render(request, 'pokemon.html', {'name': name})
